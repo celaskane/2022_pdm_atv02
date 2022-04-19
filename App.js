9662960ee029b69b7d83d4d122b050a30808195b
celaskane <leandrou9@gmail.com>
2022-04-19 13:51:06 -0300

    Iniciando projeto React Native

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      texto: "Texto inicial",
+      n1: 0,
+      n2: 0,
+      n3: 0,
+      n4: 0,
+      n5: 0,
+      n6: 0
+    }
+  }
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text>{this.state.n1}</Text>
+        <Text>{this.state.n2}</Text>
+        <Text>{this.state.n3}</Text>
+        <Text>{this.state.n4}</Text>
+        <Text>{this.state.n5}</Text>
+        <Text>{this.state.n6}</Text>
+        <Button
+          title="OK"
+          onPress={() => {
+            this.setState({n1: this.state.n1 + 1})
+            this.setState({n2: this.state.n2 +2})
+          }}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
