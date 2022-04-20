0f3577d13f1ce5bdaa1c3ab8b266e7465382cc10
celaskane <leandrou9@gmail.com>
2022-04-19 23:15:05 -0300

    Sorteio sem repetição implementado. Projeto finalizado.

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+function randomNoRepeats(array) {
+  var copy = array.slice(0);
+  return function() {
+    if (copy.length < 1) { copy = array.slice(0); }
+    var index = Math.floor(Math.random() * copy.length);
+    var item = copy[index];
+    copy.splice(index, 1);
+    return item;
+  };
 }
+var chooser = randomNoRepeats([1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                              11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                              21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+                              31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+                              41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+                              51, 52, 53, 54, 55, 56, 57, 58, 59, 60]);
 
 export default class App extends React.Component{
   constructor(props){
@@ -33,12 +44,12 @@ export default class App extends React.Component{
           title="Sorteio Megasena"
           onPress={() => {
             this.setState({
-              n1: getRandomInt(1, 61),
-              n2: getRandomInt(1, 61),
-              n3: getRandomInt(1, 61),
-              n4: getRandomInt(1, 61),
-              n5: getRandomInt(1, 61),
-              n6: getRandomInt(1, 61)
+              n1: chooser(),
+              n2: chooser(),
+              n3: chooser(),
+              n4: chooser(),
+              n5: chooser(),
+              n6: chooser()
             })
           }}
         />
